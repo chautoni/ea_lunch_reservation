13125a8dd9308997ca80c0db8caa2e9e0d5494a0
Thach Chau <rog.kane@gmail.com>
2013-03-29 10:00:00 +0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'pg'
+gem 'haml-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :production do
+  gem 'unicorn'
+end
+
+group :development do
+ gem 'debugger'
+ gem 'rspec-rails'
+end
+
+ruby '2.0.0'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EaLunchReservation::Application.config.secret_token = '11d13c59c8e43c903e0c03755bcd2357a7b2ac5c6deefadb11d2080fdc9d807d3a4987207e739467130ab52ccdb44ca557b98ac4598e501d4fcfcaaab571bb35'
