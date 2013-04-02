af5257659e008905116c1d4a5e6a146dfc3c84ad
Thach Chau <rog.kane@gmail.com>
2013-04-02 07:24:42 +0700

    Correct typo

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -4,7 +4,7 @@ class SummariesController < ApplicationController
   def today
     @summary = Summary.find_by_code(Time.zone.now.strftime('%d-%m-%Y'))
     unless @summary
-      redirect_to reservations_path, notice: 'There is no reservations for today yet!'
+      redirect_to reservations_path, notice: 'There is no reservation for today yet!'
       return
     end
   end
